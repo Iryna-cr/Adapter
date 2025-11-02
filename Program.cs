72d312c1b75282fc8c90fd4c8026576b99edf1bc
Iryna <kytsenkoiryna33@gmail.com>
2025-11-02 21:50:18 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+// Сторонній клас, який не підходить до нашої системи
+class ForeignPaymentSystem
+{
+    public void MakeForeignPay() => Console.WriteLine("Оплата через зовнішній сервіс");
+}
+
+// Наш адаптер
+class PaymentAdapter
+{
+    private ForeignPaymentSystem foreign = new ForeignPaymentSystem();
+
+    public void Pay()
+    {
+        foreign.MakeForeignPay();
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        var adapter = new PaymentAdapter();
+        adapter.Pay();
+    }
+}
+
+
+
+
+
+
+
